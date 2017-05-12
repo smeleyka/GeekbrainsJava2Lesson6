691b253fa22c019169ad84d0b89e903deaf07c85
smeleyka <smeleyka@gmail.com>
2017-05-12 19:49:06 +0300

    console

--- src/ru/geekbrains/chat/console/client/Main.java
+++ src/ru/geekbrains/chat/console/client/Main.java
@@ -1,7 +1,48 @@
 package ru.geekbrains.chat.console.client;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Created by smeleyka on 12.05.17.
  */
 public class Main {
+    static private DataInputStream in;
+    static private DataOutputStream out;
+    static private Scanner sc;
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost",6666);
+            System.out.println("Connected to server.");
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            Thread outThread = new Thread(() -> {
+                try {
+                    while (true) sendMsg(out);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            outThread.setDaemon(true);
+            outThread.start();
+            String msg = null;
+            while (!(msg=in.readUTF()).equals("end")){
+                msg = in.readUTF();
+                System.out.println("Server: "+msg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Client disconnected.");
+        }
+    }
+    public static void sendMsg(DataOutputStream out) throws IOException {
+        sc = new Scanner(System.in);
+        String outMsg = sc.nextLine();
+        out.writeUTF(outMsg);
+    }
 }
--- src/ru/geekbrains/chat/console/server/Main.java
+++ src/ru/geekbrains/chat/console/server/Main.java
@@ -13,32 +13,37 @@ import java.util.Scanner;
 public class Main {
     static private DataInputStream in;
     static private DataOutputStream out;
-    static private Scanner scin;
-
+    static private Scanner sc;
     public static void main(String[] args) {
-        try {
-            ServerSocket socketS = new ServerSocket(6666);
-            System.out.println("Server started.");
-            Socket socket = socketS.accept();
-            System.out.println("Client connected.");
-
-
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-            String msg = null;
-            System.out.println("Перед циклом.");
-
-            while (true){
-                System.out.println("Зашел в цикл.");
-                msg = in.readUTF();
-                System.out.println("Client: "+msg);
-                out.writeUTF("echo "+msg+"\n");
+            try {
+                ServerSocket socketS = new ServerSocket(6666);
+                System.out.println("Server started.");
+                Socket socket = socketS.accept();
+                System.out.println("Client connected.");
+                in = new DataInputStream(socket.getInputStream());
+                out = new DataOutputStream(socket.getOutputStream());
+                Thread outThread = new Thread(() -> {
+                    try {
+                        while (true) sendMsg(out);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                outThread.setDaemon(true);
+                outThread.start();
+                String msg = null;
+                while (true){
+                    msg = in.readUTF();
+                    System.out.println("Client: "+msg);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Client disconnected.");
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Client disconnected.");
         }
-
+    public static void sendMsg(DataOutputStream out) throws IOException {
+        sc = new Scanner(System.in);
+        String outMsg = sc.nextLine();
+        out.writeUTF(outMsg);
     }
 }
